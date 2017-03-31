e49c14e8af7e1a1bd18d05239149720ba8b066d3
Derek Wolters <derek.wolters@gmail.com>
2017-03-31 15:35:42 -0400

    Added check for words that begin with vowel

--- L6_EnglishToPigLatinTranslator/EnglishToPigLatinTranslator.cs
+++ L6_EnglishToPigLatinTranslator/EnglishToPigLatinTranslator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace L6_EnglishToPigLatinTranslator
 {
@@ -10,15 +11,15 @@ namespace L6_EnglishToPigLatinTranslator
             bool keepGoing = true;
 
             Console.WriteLine("Let's play with Pig Latin!");
-            Console.WriteLine("Enter a word or phase:");
+            //Console.WriteLine("Enter a word or phase:");
 
             while (keepGoing)
             {
                 //generate random number
-                int randNum = genRandNum();
+                //int randNum = genRandNum();
 
                 //get input, calculate and display the results
-                CheckInput(GetInput(), randNum);
+                ConvertInput(GetInput());
 
                 //exit program               
                 if (ExitProgram())
@@ -33,21 +34,58 @@ namespace L6_EnglishToPigLatinTranslator
         public static String GetInput()
         {
             string input = "";
-           
+
+            Console.WriteLine("Enter a word of phrase:");
             input = Console.ReadLine();
 
             //check that there is actually some input
             if (input == "")
             {                
-                Console.WriteLine("You need to enter a word or phrase!");
                 return GetInput();
             }
             else { return input; }
         }
 
-        public static string ConvertInput()
+        public static string ConvertInput(string phrase)
         {
-            return string phrase;
+            string[] words = Regex.Split(phrase, "\\s");
+            string[] vowels = { "a", "e", "i", "o", "u" +
+                                "A", "E", "I", "O", "U" };
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine("array:" + words[i]);
+                foreach (string x in vowels)
+                {
+                    if (words[i].Substring(0, 1).Contains(x))
+                    {
+                        Console.WriteLine("before:" + words[i]);
+                        words[i] = words[i] + "way";
+                        Console.WriteLine("after:" + words[i]);
+                    }
+                }
+            }
+            
+            //foreach (string word in words)
+            //{
+            //    Console.WriteLine("1st foreach: " + word);
+            //    foreach (string x in vowels)
+            //    {
+            //        Console.WriteLine("vowel:" + x);
+            //        Console.WriteLine("2nd foreach: " + word);
+            //        Console.WriteLine("substring: " + word.Substring(0, 1));
+            //        if (word.Substring(0,1).Contains(x))
+            //        {                        
+            //            Console.WriteLine("before:" + word);
+            //            word.Replace(word , word + "way");
+            //            Console.WriteLine("after:" + word);
+            //        }
+            //    }
+            //}
+
+            
+
+            return phrase;
         }
 
         //exit the program when the user wants to
