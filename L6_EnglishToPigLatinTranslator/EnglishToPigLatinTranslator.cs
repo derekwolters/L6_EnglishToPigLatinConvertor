864a3321673159bac0dccbd77337d5f9f3007a96
Derek Wolters <derek.wolters@gmail.com>
2017-03-31 10:31:26 -0400

    Added prebuilt methods

--- L6_EnglishToPigLatinTranslator/EnglishToPigLatinTranslator.cs
+++ L6_EnglishToPigLatinTranslator/EnglishToPigLatinTranslator.cs
@@ -6,10 +6,76 @@ using System.Threading.Tasks;
 
 namespace L6_EnglishToPigLatinTranslator
 {
-    class Program
+    class EnglishToPigLatinTranslator
     {
         static void Main(string[] args)
         {
+            //initialize variables
+            bool keepGoing = true;
+
+            Console.WriteLine("Let's play with Pig Latin!");
+            Console.WriteLine("Enter a word or phase:");
+
+            while (keepGoing)
+            {
+                //generate random number
+                int randNum = genRandNum();
+
+                //get input, calculate and display the results
+                CheckInput(GetInput(), randNum);
+
+                //exit program               
+                if (ExitProgram())
+                {
+                    Console.WriteLine("Goodbye!");
+                    System.Threading.Thread.Sleep(1000);
+                    break;
+                }
+            }
+        }
+
+        public static int GetInput()
+        {
+            string input = "";
+            int temp;
+
+            input = Console.ReadLine();
+
+            if (!int.TryParse(input, out temp))
+            {
+                //check that input is an integer & ask for reentry if not
+                Console.WriteLine("Input should be a whole number. " +
+                    "Try again.");
+                return GetInput();
+            }
+            else if (temp < minNum || temp > maxNum)
+            {
+                //check that input is withing range & ask for reentry if not
+                Console.WriteLine("Input should between 1 and 100");
+                return GetInput();
+            }
+            else { return temp; }
+        }
+
+        public static string ConvertInput()
+        {
+            return string phrase;
+        }
+
+        //exit the program when the user wants to
+        public static Boolean ExitProgram()
+        {
+            Console.WriteLine("Do you want to continue? Enter Y or N.");
+            var KP = Console.ReadKey(true);
+
+            while (KP.Key != ConsoleKey.N && KP.Key != ConsoleKey.Y)
+            {
+                Console.WriteLine("Not a vaid answer. Do you want to " +
+                    "continue? Enter Y or N.");
+                KP = Console.ReadKey(true);
+            }
+
+            return KP.Key == ConsoleKey.N ? true : false;
         }
     }
 }
